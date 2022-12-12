6c5b80aeb53fb4ebe2701c0132bb09da57a8f84a
Claudio Pinna <claudiopinna@live.com>
2022-12-12 21:54:51 +0100

    Update D6.js

--- D6.js
+++ D6.js
@@ -10,6 +10,7 @@ REGOLE
 /* ESERCIZIO 1
  Scrivi una funzione di nome "area", che riceve due parametri (l1, l2) e calcola l'area del rettangolo associato..
 */
+
 console.log("-------- Esercizio 1 --------");
 
 function area(l1, l2) {
@@ -85,48 +86,111 @@ console.log(q);
 
 console.log("-------- Esercizio 5 --------");
 
-/*function epify(e) {
-  if (typeof x !== "number") {
-    return true;
-    if else ()
+function epify(str) {
+  if (str.startsWith("EPICODE")) {
+    return str;
   } else {
-    return false;
+    return "EPICODE " + str;
   }
 }
-const h = 19;
-const q = boundary(h);
 
-console.log(q);
+let frase = epify("saluti!");
+//let frase = epify("EPICODE saluti!");
+console.log(frase);
 
 /* ESERCIZIO 6
  Scrivi una funzione di nome "check3and7" che accetta un numero positivo come parametro. La funzione deve controllare che il parametro sia un multiplo
  di 3 o di 7. (Suggerimento: usa l'operatore modulo)
 */
+console.log("-------- Esercizio 6 --------");
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+function check3and7(n) {
+  if (typeof n !== "number" || n < 0) {
+    risultato = ": Il valore inserito non è un numero o è un numero negativo";
+  } else {
+    switch (0) {
+      case n % 3:
+        risultato = ": Il valore inserito è un multiplo di 3";
+        break;
+      case n % 7:
+        risultato = ": Il valore inserito è un multiplo di 7";
+        break;
+      default:
+        risultato = ": Il valore inserito non è multiplo di 3 o di 7";
+    }
+  }
+  return risultato;
+}
+
+const n = 10;
+//const n = -10;
+//const n = 14
+//const n = 18
+//const n = "gattino"
+console.log(n + check3and7(n));
 
 /* ESERCIZIO 7
  Scrivi una funzione di nome "reverseString", il cui scopo è invertire una stringa fornita come parametro (es. "EPICODE" --> "EDOCIPE")
 */
+console.log("-------- Esercizio 7 --------");
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+function reverseString(myStr) {
+  return myStr.split("").reverse().join("");
+}
+const myStr = "EPICODE SALUTI";
+
+console.log(reverseString(myStr));
 
 /* ESERCIZIO 8
  Scrivi una funzione di nome "upperFirst", che riceve come parametro una stringa formata da diverse parole.
  La funzione deve rendere maiuscola la prima lettera di ogni parola contenuta nella stringa.
 */
+console.log("-------- Esercizio 8 --------"); //NON FUNZIONA RITORNACI
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+function upperFirst(string1) {
+  const splitstring = string1.split(" ");
+  for (let i = 0; i < splitstring.lenght; i++) {
+    splitstring[i] =
+      splitstring[i].charAt(0).toUppercase() + splitstring[i].substring(1);
+  }
+  return splitstring.join(" ");
+}
+let upperedWords = upperFirst("Stringa di diverse parole");
+console.log(upperedWords);
 
 /* ESERCIZIO 9
  Scrivi una funzione di nome "cutString", che riceve come parametro una stringa. La funzione deve creare una nuova stringa senza il primo e l'ultimo carattere
  della stringa originale.
 */
+console.log("-------- Esercizio 9--------");
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+function cutString(string) {
+  if (typeof string === "string") {
+    stringF = string.slice(1, -1);
+    console.log(stringF);
+  } else {
+    console.log("inserisci stringa");
+  }
+}
+cutString("Salute e vita");
 
 /* ESERCIZIO 10
  Scrivi una funzione di nome "giveMeRandom", che accetta come parametro un numero n e ritorna un'array contenente n numeri casuali inclusi tra 0 e 10.
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+console.log("-------- Esercizio 10 --------");
+
+function giveMeRandom(n) {
+  if (typeof n !== "number") {
+    return "Non è stato inserito un numero";
+  } else {
+    let rng = [];
+    for (let i = 0; i < n; i++) {
+      rng.push(Math.floor(Math.random() * 10 + 1));
+    }
+
+    return rng;
+  }
+}
+const eG = giveMeRandom(2);
+console.log(eG);
